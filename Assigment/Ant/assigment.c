aab5dbae638b7fb3cb5d75b33c488c3946cd8d13
Anthony Chew <anthony.chew@hotmail.com>
2021-10-19 20:53:37 +0800

    Assignment AddCardFunction

--- Assigment/Ant/assigment.c
+++ Assigment/Ant/assigment.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
 
+#define true 0
+#define false !true
+
 //NameCard struct
 struct NameCard
 {
- int nameCardID;
- char personName[20];
- char companyName[20];
+    int nameCardID;
+    char personName[20];
+    char companyName[20];
 };
 
 //Functions
-void choiceFromUser(int *userChoice); 
-void listNameCards(struct NameCard allNameCards[5] , int *cardsEmptyFlag);
+void choiceFromUser(int *userChoice);
+void listNameCards(struct NameCard allNameCards[5], int *cardsCount);
+int addNameCard(struct NameCard allNameCards[5], int *cardCount);
 
 //Main Func
 int main()
 {
     //Initilization
-    struct NameCard allNameCards[5] ; //NameCard Array with 5 max capicity
-    int userChoice = 999 , cardsEmptyFlag = 0; // User choice , Flag to check is card empty
+    struct NameCard allNameCards[5];       //NameCard Array with 5 max capicity
+    int userChoice = 999, cardsCount = -1; // User choice , Flag to check is card empty
 
     //Test case
     //struct NameCard allNameCards[5] ={ {9,"PC Leong","NTU" } , {5,"WC Tan","NTU"} ,{7,"Philip Fu","NTU"} };
-    
+
     //Welcome msg
     printf("NTU NAME CARD HOLDER MANAGEMENT PROGRAM:\n"
-            "1: listNameCards() \n"
-            "2: addNameCard() \n"
-            "3: removeNameCard() \n"
-            "4: getNameCard()  \n"
-            "5: quit \n");
-    
+           "1: listNameCards() \n"
+           "2: addNameCard() \n"
+           "3: removeNameCard() \n"
+           "4: getNameCard()  \n"
+           "5: quit \n");
+
     //Keep looping until exit is 5
     while (1)
     {
@@ -40,11 +44,13 @@ int main()
         {
 
         case 1:
-            listNameCards(allNameCards , &cardsEmptyFlag);
+            printf("listNameCards(): \n");
+            listNameCards(allNameCards, &cardsCount);
             break;
 
         case 2:
-            /* code */
+            printf("addNameCard(): \n");
+            addNameCard(allNameCards, &cardsCount);
             break;
 
         case 3:
@@ -54,18 +60,17 @@ int main()
         case 4:
             /* code */
             break;
-        
+
         //Exit when 5 is input
         case 5:
             printf("Exited!");
             return 0;
-        
+
         //Anything else just ask again
         default:
             continue;
         }
     }
-    
 
     return 0;
 }
@@ -79,27 +84,82 @@ void choiceFromUser(int *userChoice)
 }
 
 //List all the card in struct array
-void listNameCards(struct NameCard allNameCards[5] , int *cardsEmptyFlag)
+void listNameCards(struct NameCard allNameCards[5], int *cardsCount)
 {
-    //If card is empty 
-    if(*cardsEmptyFlag == 1)
+    //If card is empty
+    if (*cardsCount == -1)
     {
         printf("The name card holder is empty\n");
     }
     else
     {
         //Loop through all index of allCards
-        for(int i = 0; i< 4; i++)
+        for (int i = 0; i < 5; i++)
         {
             //If all the input is default don't show
-            if(allNameCards[i].nameCardID != 0 && allNameCards[i].personName != "" && allNameCards[i].companyName != "")
+            if (allNameCards[i].nameCardID != 0 && allNameCards[i].personName != "" && allNameCards[i].companyName != "")
             {
                 printf("nameCardID: %d\n"
-                        "personName: %s\n"
-                        "companyName: %s\n" 
-                        , allNameCards[i].nameCardID , allNameCards[i].personName , allNameCards[i].companyName);
+                       "personName: %s\n"
+                       "companyName: %s\n",
+                       allNameCards[i].nameCardID, allNameCards[i].personName, allNameCards[i].companyName);
             }
         }
-
     }
+}
+
+int addNameCard(struct NameCard allNameCards[5], int *cardCount)
+{
+    *cardCount += 1; //current card amount
+    int cardID, cardExistFlag = false;// CardID for user input, Card Exist Flag to check does card exist
+    char personName[20], companyName[20];// PersonName and CompanyName for user input 
+
+    //Ask for user input
+    printf("Enter nameCardID:\n");
+    scanf("%d", cardID);
+
+    printf("Enter personName:\n");
+    scanf("%s", &personName[20]);
+
+    printf("Enter companyName:\n");
+    scanf("%s", &companyName[20]);
+
+    //If card less than 4
+    if (cardCount <= 4)
+    {
+        //Loop through all index of allCards
+        for (int i = 0; i < 5; i++)
+        {
+            //If any duplicated NameCardID
+            if (allNameCards[i].nameCardID == cardID)
+            {   
+                //Set Flag to true
+                cardExistFlag = true;
+            }
+        }
+        
+        //If card not exist
+        if (cardExistFlag == false)
+        {
+            allNameCards[*cardCount].nameCardID = cardID;
+
+            *allNameCards[*cardCount].personName = personName[20];
+
+            *allNameCards[*cardCount].companyName = companyName[20];
+
+            return true;
+        }
+        //Else reduce count to the previous and return false
+        else
+        {
+            *cardCount -= 1;
+            return false;
+        }
+    }
+    //Else exit
+    else
+    {
+        return false;
+    }
+    
 }
